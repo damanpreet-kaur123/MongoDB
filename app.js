5c13104fd477673f6ea70b87e82dd46830826883
damanpreet-kaur123 <155047827+damanpreet-kaur123@users.noreply.github.com>
2026-03-29 22:20:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import myRoute from "./routes/route.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+const app = express();
+
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB connected via Mongoose"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use("/api", myRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
